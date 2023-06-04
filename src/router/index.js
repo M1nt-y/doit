f959fd67fd0bd0f29d38188e7ff4f09880494469
HunterAkeriano <stajectgamer@gmail.com>
2023-06-04 16:43:17 +0300

    diva-dev: 404 Desktop

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/pages/HomePage.vue'
+import NotFound from '@/pages/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomePage
+    },
+    {
+      name: 'Not Found',
+      path: '/:pathMatch(.*)*',
+      component: NotFound
     }
   ]
 })
