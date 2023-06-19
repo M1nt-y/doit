f261853272deb6f1c1b03d303670d89c8ce6b84e
HunterAkeriano <stajectgamer@gmail.com>
2023-06-19 20:28:53 +0300

    dima-dev: Добавление блока с ценниками, адаптация года и ценников

--- src/pages/TopPage.vue
+++ src/pages/TopPage.vue
@@ -14,7 +14,7 @@
       </div>
       <div class="top__price">
         <div class="price">
-          <h4>Total Tournaments: <span>157,260,352</span></h4>
+          <h4>Total Tournaments: <span class="price-event">157,260,352</span></h4>
         </div>
         <div class="price">
           <h4>Total Prize money <span>$157,260,352.09</span></h4>
@@ -44,10 +44,19 @@ const resultYear = yearsArray.value[0]
 </script>
 
 <style lang="scss">
-
+@import '@/assets/scss/variables.scss';
+@import '@/assets/scss/media-breakpoints.scss';
 .top {
+  .container{
+    @include media-breakpoint-down(xs) {
+      padding: 0 14px;
+    }
+  }
   &__title {
     margin-top: 63px;
+    @include media-breakpoint-down(xs) {
+      margin-top: 23px;
+    }
     h2{
       display: flex;
       align-items: center;
@@ -60,6 +69,13 @@ const resultYear = yearsArray.value[0]
       color: #F5F5F5;
       gap: 10px;
       margin-left: -5px;
+      @include media-breakpoint-down(xs) {
+        font-weight: 700;
+        font-size: 28px;
+        line-height: 100%;
+        margin-left: -10px;
+        gap: 5px;
+      }
     }
     &-select {
       display: flex;
@@ -75,9 +91,20 @@ const resultYear = yearsArray.value[0]
         margin-right: 9px;
         margin-top: 2px;
         cursor: pointer;
+        @include media-breakpoint-down(xs) {
+          font-weight: 700;
+          font-size: 28px;
+          line-height: 100%;
+          // margin-right: 5px;
+          // margin-left: 0px;
+        }
       }
       svg{
         transition: .15s;
+        @include media-breakpoint-down(xs) {
+          width: 12px;
+          height: 12px;
+        }
       }
       svg.active{
         transform: rotate(180deg);
@@ -86,8 +113,10 @@ const resultYear = yearsArray.value[0]
         position: absolute;
         top: 50px;
         width: 80%;
+        background: black;
         p{
           font-size: 30px;
+          margin-top: 5px;
           cursor: pointer;
           &:hover{
             background-color: #2787F6;
@@ -100,6 +129,55 @@ const resultYear = yearsArray.value[0]
     display: flex;
     justify-content: space-between;
     margin-top: 50px;
+    @include media-breakpoint-down(xs) {
+      margin-top: 31px;
+    }
+    h4{
+      font-style: normal;
+      font-weight: 700;
+      font-size: 22px;
+      line-height: 100%;
+      color: #E6E8EB;
+      display: flex;
+      align-items: center;
+      gap: 18px;
+      span{
+        font-weight: 400;
+        font-size: 22px;
+        line-height: 100%;
+        /* identical to box height, or 22px */
+
+        text-align: right;
+
+        color: #8995A3;
+        @include media-breakpoint-down(xs) {
+          font-weight: 500;
+          font-size: 16px;
+          line-height: 100%;
+          transform: translateX(2px);
+        }
+
+      }
+      .price-event{
+        @include media-breakpoint-down(xs) {
+          transform: translateX(2px);
+          letter-spacing: 1px;
+        }
+      }
+      @include media-breakpoint-down(lg) {
+        display: flex;
+        justify-content: space-between;
+      }
+      @include media-breakpoint-down(xs) {
+        font-weight: 500;
+        font-size: 16px;
+        line-height: 100%;
+      }
+    }
+    @include media-breakpoint-down(lg) {
+      flex-direction: column-reverse;
+      gap: 18px;
+    }
   }
 }
 </style>
