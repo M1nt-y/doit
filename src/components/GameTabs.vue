c4e3e6116dc75d2c54f1f7a99571d54a857a8bbe
M1nt-y <sviatoslav.ivashchenko@student.sumdu.edu.ua>
2023-06-20 21:17:03 +0300

    responsive game tabs

---
+++ src/components/GameTabs.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="tabs">
+    <div
+        v-for="(tab, i) in gameTabs" :key="i"
+        class="tab"
+        :class="{ 'tab--active' : tab === active }"
+        @click="active = tab"
+    >
+      {{ tab }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+
+
+const gameTabs = ['All', 'StarCraft II', 'Dota II', 'CS:GO', 'LOL', 'Fortnite']
+const active = ref('Dota II')
+</script>
+
+<style scoped lang="scss">
+.tabs {
+  display: flex;
+  margin-top: 2px;
+  flex-wrap: wrap;
+  margin-left: auto;
+  justify-content: flex-end;
+}
+
+.tab {
+  padding: 16px;
+  color: #A0A5AD;
+  font-size: 18px;
+  cursor: pointer;
+  font-weight: 500;
+  margin-left: 2px;
+  line-height: 24px;
+  margin-bottom: 2px;
+  background: #14191F;
+  transition: all 0.3s ease;
+
+  &--active,
+  &:hover {
+    color: #0F1215;
+    background: #D8DFEB;
+  }
+}
+</style>
